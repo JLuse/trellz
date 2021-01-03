ef3586518f96dad8d23905f63341fa11bef9d162
Jordan Luse <j.luse7@gmail.com>
2021-01-02 19:20:20 -0800

    Refactored to use functional component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react';
-import List from './List'
+import React from 'react';
+import List from './composition/List';
 import './App.css';
 
-class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
 
-  render() {
-    const { store } = this.props
+export default function App(props) {
+    // console.log(props.store );
     return (
-      <main className='App'>
-        <header className='App-header'>
-          <h1>Trellz</h1>
-        </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
-            <List
-              key={list.id}
-              header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
-            />
-          ))}
-        </div>
-      </main>
+        <main className='App'>
+            <header className='App-header'>
+                <h1>Trellz</h1>
+            </header>
+            <div className='App-list'>
+                {props.store.lists.map(list => (
+                    <List 
+                        key={list.id}
+                        title={list.header}
+                        // combines the cardIds array for the list with the allCards object
+                        cards={list.cardIds.map(id => (props.store.allCards[id]))}
+                    />
+                ))}
+            </div>
+        </main>
     );
-  }
 }
-
-export default App;
