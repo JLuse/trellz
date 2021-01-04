140414bed7808ce0b5f5cebbe09c5c052f5d6e84
Jordan Luse <j.luse7@gmail.com>
2021-01-03 18:05:16 -0800

    Added basic smoke and snapshot test for List and Card component

---
+++ src/composition/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Card from './Card';
+
+describe('Card component', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Card />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('render the UI as expected', () => {
+        const tree = renderer
+            .create(<Card title='Title' content='Some Content' />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
